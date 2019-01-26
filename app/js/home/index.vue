2bcc17f023a2ce66a2ed0da1f8d8e4aa08ae4e30
1797172507@qq.com <1797172506@qq.com>
2019-01-26 13:18:48 +0800

    footer

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -7,17 +7,22 @@
       <Money/>
       <Product/>
       <Left/>
+      <Footer/>
+      <Navbar/>
     </div>
 </template>
 
 <script>
 import Heador from '../public/header.vue'
+import Footer from '../public/footer.vue'
+import Navbar from '../public/navbar.vue'
 import Hslider from './hslider.vue'
 import Novice from './novice.vue'
 import Borrow from './borrow.vue'
 import Money from './money.vue'
 import Product from './product.vue'
 import Left from './left.vue'
+
 export default {
   components: {
     Heador,
@@ -26,7 +31,9 @@ export default {
    Borrow,
    Money,
    Product,
-   Left
+   Left,
+   Footer,
+   Navbar
   }
 }
 </script>
